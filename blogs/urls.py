2b38abffe441b8746889f80e7d1c6816fa00b842
prajwaldankit <prajwal.x82@gmail.com>
2025-07-14 10:51:48 +0545

    Added category app.

--- blogs/urls.py
+++ blogs/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include("post.urls")),
+    path('category/', include("category.urls")),
     path('admin/', admin.site.urls),
 ]
