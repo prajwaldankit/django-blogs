111855d962701647c5a64520c19ff664017d5068
prajwaldankit <prajwal.x82@gmail.com>
2025-07-17 15:06:57 +0545

    Added autogenerated slug in post.

--- post/models.py
+++ post/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from category.models import Category
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -14,6 +15,16 @@ class Post(models.Model):
 
     class Meta:
         db_table = 'posts'
+
+    def save(self, *args, **kwargs):
+        base_slug = slugify(self.title)
+        slug = base_slug
+        counter = 1
+        while Post.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+            slug = f"{base_slug}-{counter}"
+            counter += 1
+        self.slug = slug
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
