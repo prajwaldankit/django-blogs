d106154cbc1dea9abfcf4129b3067c887b13de17
prajwaldankit <prajwal.x82@gmail.com>
2025-07-22 09:45:41 +0545

    Added comment functionality.

--- post/urls.py
+++ post/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.index, name="post_homepage"),
     path('id/<int:id>/', views.post_detail_by_id, name="post_detail"),
     # path('<str:slug>/', views.post_detail_by_slug, name="post_by_slug")
-    path('<str:slug>/', views.PostDetailView.as_view(), name="post_by_slug")
+    path('<str:slug>/', views.PostDetailView.as_view(), name="post_by_slug"),
+    path('comment/<int:pk>/edit/',
+         views.CommentUpdateView.as_view(), name='comment_edit'),
+    path('comment/<int:pk>/delete/',
+         views.CommentDeleteView.as_view(), name='comment_delete'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import DetailView
-from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import DetailView, UpdateView, DeleteView
+from django.shortcuts import render, redirect
 from .models import Post, Comment
 from .forms import CommentForm
 # Create your views here.
@@ -39,5 +41,39 @@ class PostDetailView(DetailView):
         return context
 
     def post(self, request, *args, **kwargs):
-        # TODO: implement saving comments.
-        return None
+        self.object = self.get_object()
+        form = CommentForm(request.POST)
+
+        if request.user.is_authenticated and form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = self.object
+            comment.user = request.user
+            comment.save()
+            return redirect('post_by_slug', slug=self.object.slug)
+
+        context = self.get_context_data()
+        context['form'] = form
+        return self.render_to_response(context)
+
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'post/edit_comment.html'
+
+    def get_success_url(self):
+        return reverse('post_by_slug', kwargs={'slug': self.object.post.slug})
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
+
+
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = 'post/delete_comment.html'
+
+    def get_success_url(self):
+        return reverse('post_by_slug', kwargs={'slug': self.object.post.slug})
+
+    def test_func(self):
+        return self.request.user.is_superuser
