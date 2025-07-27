96d61e1622877361d7b9a2bf84905714343b39d1
prajwaldankit <prajwal.x82@gmail.com>
2025-07-27 13:26:11 +0545

    Fixed tailwind issue and added components.

--- components/post_card/post_card.py
+++ components/post_card/post_card.py
@@ -1,11 +1,11 @@
 from django_components import Component, register
 
 
-@register("category_card")
-class CategoryCard(Component):
-    template_file = "category_card.html"
+@register("post_card")
+class PostCard(Component):
+    template_file = "post_card.html"
 
     def get_template_data(self, args, kwargs, slots, context):
         return {
-            "category": kwargs["category"]
+            "post": kwargs["post"]
         }
